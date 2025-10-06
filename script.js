1cd6d2db42d5040165d5634bbbd7ef698e5bf622
singhutp-glitch <singhutp@gmail.com>
2025-10-06 19:40:36 +0530

    Add function to create cards
    -create card of all books in library
    -but do not display it yet

--- script.js
+++ script.js
@@ -16,8 +16,35 @@ function addToLib(title, author, pages, read)
     myLib.push(book);
 }
 
+function display(myLib)
+{   
+
+    for (let book of myLib)
+    { 
+        const card=document.createElement('div');
+        const bookTitle=document.createElement('div');
+        const bookAuthor=document.createElement('div');
+        const bookPages=document.createElement('div');
+        const bookRead=document.createElement('div');
+        bookTitle.textContent=book.title;
+        bookAuthor.textContent=book.author;
+        bookPages.textContent=book.pages;
+        bookRead.textContent=book.read;
+        card.appendChild(bookTitle);
+        card.appendChild(bookAuthor);
+        card.appendChild(bookPages);
+        card.appendChild(bookRead);
+    }
+
+}
+
 //main
 
+const bodyElement=document.querySelector('body');
+
 myLib=[];
-addToLib("atomic","me",1,1);
-console.log(myLib[0]);
+addToLib("b1",'a1',1,1);
+addToLib("b2","a2",1,1);
+addToLib("b3","a3",1,1);
+
+display(myLib);
