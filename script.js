9bc0fa6536bc9e23c912a7e3e40c42bf7713ead0
singhutp-glitch <singhutp@gmail.com>
2025-10-06 18:52:04 +0530

    Add Book create and add functions

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("hello");
+
+//functions
+
+function Book(title, author, pages, read)
+{
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read=read;
+}
+
+function addToLib(title, author, pages, read)
+{
+    const book= new Book(title, author, pages, read);
+    myLib.push(book);
+}
+
+//main
+
+myLib=[];
+addToLib("atomic","me",1,1);
+console.log(myLib[0]);
