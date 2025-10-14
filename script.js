5918148e894e3df59b6ecba73f706c2853b30f95
singhutp-glitch <singhutp@gmail.com>
2025-10-14 11:48:16 +0530

    Convert constructor to class

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //functions
-
-function Book(title, author, pages, read)
-{
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read;
+class Book{
+    constructor(title, author, pages, read)
+    {
+        this.title=title;
+        this.author=author;
+        this.pages=pages;
+        this.read=read;
+    }
 }
 
 function addToLib(title, author, pages, read)
